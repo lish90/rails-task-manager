3a0f13153fa55809eb3c344ae36c2c2596579c13
alisha musamabik <alisha.musamabik@gmail.com>
2020-02-08 18:00:56 +0000

    updating frontend

--- config/routes.rb
+++ config/routes.rb
@@ -10,19 +10,4 @@ Rails.application.routes.draw do
   patch 'tasks/:id', to: 'tasks#update'
 
   delete 'tasks/:id', to: 'tasks#destroy'
-
-  # resources :tasks
 end
-
-# get    "restaurants",          to: "restaurants#index"
-
-  # get    "restaurants/new",      to: "restaurants#new"
-  # post   "restaurants",          to: "restaurants#create"
-
-  # # NB: The `show` route needs to be *after* `new` route.
-  # get    "restaurants/:id",      to: "restaurants#show"
-
-  # get    "restaurants/:id/edit", to: "restaurants#edit"
-  # patch  "restaurants/:id",      to: "restaurants#update"
-
-  # delete "restaurants/:id",      to: "restaurants#destroy"
